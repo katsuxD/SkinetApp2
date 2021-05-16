e3934966b20aedfa90e4169a968aeadaa20971e9
katsuxD <khanhb5192@gmail.com>
2021-05-16 23:28:20 +0700

    setting up the error notification when user make a bad request or another error - error notification will be display by using ngx-toastr@13.2.1

---
+++ client/src/app/core/test-error/test-error.component.ts
@@ -0,0 +1,54 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-test-error',
+  templateUrl: './test-error.component.html',
+  styleUrls: ['./test-error.component.scss']
+})
+export class TestErrorComponent implements OnInit {
+  baseUrl = environment.apiUrl;
+  validationErrors: any;
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+  // tslint:disable-next-line: typedef
+  get404Error() {
+    this.http.get(this.baseUrl + 'products/42').subscribe(respone => {
+      console.log(respone);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  // tslint:disable-next-line: typedef
+  get500Error() {
+    this.http.get(this.baseUrl + 'buggy/servererror').subscribe(respone => {
+      console.log(respone);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  // tslint:disable-next-line: typedef
+  get400Error() {
+    this.http.get(this.baseUrl + 'buggy/badrequest').subscribe(respone => {
+      console.log(respone);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  // tslint:disable-next-line: typedef
+  get400ValidationError() {
+    this.http.get(this.baseUrl + 'products/fortytwo').subscribe(respone => {
+      console.log(respone);
+    }, error => {
+      console.log(error);
+      this.validationErrors = error.errors;
+    });
+  }
+}
