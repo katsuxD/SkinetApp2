fa4fab303ad1d91c3b2fe1e3be2007f7eaab19c8
katsuxD <khanhb5192@gmail.com>
2021-05-18 15:14:13 +0700

    setting up the api for basket and config redis to save basket data in the memory of server

--- API/Startup.cs
+++ API/Startup.cs
@@ -8,6 +8,7 @@ using API.Helpers;
 using AutoMapper;
 using API.Middleware;
 using API.Extensions;
+using StackExchange.Redis;
 
 namespace API
 {
@@ -28,6 +29,14 @@ namespace API
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => 
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+            
+            services.AddSingleton<IConnectionMultiplexer>(
+                c => {
+                    var configuration = ConfigurationOptions.Parse(_config.GetConnectionString("Redis"),
+                    true);
+                    return ConnectionMultiplexer.Connect(configuration);
+                }
+            );
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
             services.AddCors(opt => {
