6cfbedad01bf95a6f45f089bbaa1d9a27650194b
katsuxD <khanhb5192@gmail.com>
2021-05-18 10:57:13 +0700

    finish setting spinner and home page, install ngx-bootwatch to have more themes options

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { error } from 'selenium-webdriver';
 import { IProduct } from 'src/app/shared/models/product';
+import { BreadcrumbService } from 'xng-breadcrumb';
 import { ShopService } from '../shop.service';
 
 @Component({
@@ -11,7 +12,10 @@ import { ShopService } from '../shop.service';
 })
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
-  constructor(private shopService: ShopService, private activateRoute: ActivatedRoute) { }
+  constructor(private shopService: ShopService, private activateRoute: ActivatedRoute,
+              private bcService: BreadcrumbService) {
+                this.bcService.set('@productDetails', ' ');
+               }
 
   // tslint:disable-next-line: typedef
   ngOnInit(){
@@ -22,6 +26,7 @@ export class ProductDetailsComponent implements OnInit {
   loadProduct() {
     this.shopService.getProduct(+this.activateRoute.snapshot.paramMap.get('id')).subscribe(product => {
       this.product = product;
+      this.bcService.set('@productDetails', product.name);
     // tslint:disable-next-line: no-shadowed-variable
     }, error => {
       console.log(error);
